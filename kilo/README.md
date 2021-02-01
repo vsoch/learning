3daadd3081e6a755bf5aa03ff7cc6ba38ea7b93e
vsoch <vsoch@users.noreply.github.com>
2021-02-01 09:27:22 -0700

    adding editor writing area and rows
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- kilo/README.md
+++ kilo/README.md
@@ -2,4 +2,4 @@
 
 Designing a text editor per the instructions [here](https://viewsourcecode.org/snaptoken/kilo/01.setup.html).
 
-Stopped at this section https://viewsourcecode.org/snaptoken/kilo/02.enteringRawMode.html#turn-off-all-output-processing
+Stopped at this section https://viewsourcecode.org/snaptoken/kilo/03.rawInputAndOutput.html#clear-the-screen
--- kilo/kilo.c
+++ kilo/kilo.c
@@ -1,49 +1,111 @@
+/*** includes ***/
+
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+
+/*** defines ***/
+
+// We want control W to quit
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+/*** data ***/
+
 // Global structure of original terminal
 struct termios orig_termios;
 
+
+/*** terminal ***/
+
+// Handle error
+void die(const char *s) {
+    perror(s);
+    exit(1);
+}
+
 // disableRawMode when the editor exits
 void disableRawMode() {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+        die("tcsetattr");
 }
 
 // enableRawMode when the editor starts
 void enableRawMode() {
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
     atexit(disableRawMode);
     
     // Create a copy of the original terminal to edit
     struct termios raw = orig_termios;
 
+    // Traditional flag to disable during raw mode
     // Fix Control-M and Disable Control-S and Control-Q
     // I == input, XON == "X ON"
-    raw.c_iflag &= ~(ICRNL | IXON);
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_cflag |= (CS8);
 
+    // Turn off output processing
+    raw.c_oflag &= ~(OPOST);
+    
     // Turn off echo, canonical (read bytes instead of lines), Control-V, and SIGINT/SIGSTOP commands
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    // set a timeout so read returns if no input for a while
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
+
+char editorReadKey() {
+    int nread;
+    char c;
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+        if (nread == -1 && errno != EAGAIN) die("read");
+    }
+    return c;
+}
+
+/*** output ***/
+void editorDrawRows() {
+    int y;
+    for (y = 0; y<24; y++){
+        write(STDOUT_FILENO, "~\r\n", 3);
+    }
+}
+
+void editorRefreshScreen() {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+    editorDrawRows();
+
+    write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+
+void editorProcessKeypress() {
+    char c = editorReadKey();
+
+    switch (c) {
+        case CTRL_KEY('q'):
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);
+            exit(0);
+            break;
+    }
+}
+
+/*** init ***/
 
 int main() {
     enableRawMode();
 
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c!= 'q') {
-
-        // If we have a control byte like ENTER
-        if (iscntrl(c)) {
-            printf("%d\n", c);
-
-        // Print the byte as a number and character
-        } else {
-            printf("%d ('%c')\n", c, c);
-        }
+    while (1) {
+        editorRefreshScreen();
+        editorProcessKeypress();
     }
     return 0;
 }
